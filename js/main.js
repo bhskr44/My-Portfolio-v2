b1eae0308adfb59cd68aa983a021cd9b73383889
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-10-11 02:23:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,35 +3,30 @@ const mainElement = document.getElementById('section1');
 const navId = document.getElementById('navId');
 const menuIcon = document.getElementById('hm-menuIcon');
 const logoText = document.getElementById('logo-text');
-const mobileMenu = document.getElementById('mobile-menu');
-// const mobileMenu = document.getElementById('mobile-menu');
+const mobileNavigation = document.querySelectorAll('.clicked-item');
 
-function toggleMenu() {
+mobileNavigation.forEach((item) => {
+  item.addEventListener('click', () => {
+    menuButton.style.display = 'none';
+    navId.classList.remove('add-bg-to-nav');
+    mainElement.classList.remove('add-blur');
+    logoText.classList.remove('add-blur');
+    menuIcon.src = 'image/close.svg';
+  });
+});
+
+menuIcon.addEventListener('click', () => {
   if (menuButton.style.display === 'block') {
     menuButton.style.display = 'none';
     navId.classList.remove('add-bg-to-nav');
-    menuIcon.className = 'fa fa-bars fa-2x';
-    menuIcon.style.color = '#6070ffc2';
+    menuIcon.src = 'image/hamburger.svg';
     mainElement.classList.remove('add-blur');
     logoText.classList.remove('add-blur');
   } else {
     menuButton.style.display = 'block';
-    menuIcon.className = 'fa fa-close fa-2x';
-    menuIcon.style.color = 'white';
+    menuIcon.src = 'image/close.svg';
     navId.classList.add('add-bg-to-nav');
     mainElement.classList.add('add-blur');
     logoText.classList.add('add-blur');
   }
-}
-/* eslint-disable */
-function navigate(loc) {
-  menuButton.style.display = 'none';
-  navId.classList.remove('add-bg-to-nav');
-  mainElement.classList.remove('add-blur');
-  logoText.classList.remove('add-blur');
-  menuIcon.className = 'fa fa-bars fa-2x';
-  menuIcon.style.color = '#6070ffc2';
-  window.href = loc;
-}
-/* eslint-enable */
-mobileMenu.addEventListener('click', toggleMenu());
+});
